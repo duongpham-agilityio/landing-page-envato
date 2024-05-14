0bb81f3505383ab946463456ed66fa5163fa58e1
Ngoc Ngo Thi <ngoc.ngo@asnet.com.vn>
2024-05-14 15:52:27 +0700

    feat: add type for props of calendar

--- dashboard/app/ui/sections/Calendar/index.tsx
+++ dashboard/app/ui/sections/Calendar/index.tsx
@@ -1,13 +1,20 @@
-import { Calendar, momentLocalizer } from 'react-big-calendar';
+'use client';
+
+import {
+  Calendar as BigCalendar,
+  momentLocalizer,
+  CalendarProps,
+} from 'react-big-calendar';
 import moment from 'moment';
 
 // Setup the localizer by providing the moment (or globalize, or Luxon) Object
 // to the correct localizer.
 const localizer = momentLocalizer(moment); // or globalizeLocalizer
 
-const MyCalendar = () => (
+const MyCalendar = (props: Omit<CalendarProps, 'localizer'>) => (
   <div className="myCustomHeight">
-    <Calendar
+    <BigCalendar
+      {...props}
       localizer={localizer}
       // events={myEventsList}
       startAccessor="start"
